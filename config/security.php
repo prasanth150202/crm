db89d2a4c6cdfc7dae580cf782e7f89ef2a125c2
Prasanth <prasanthsk1502@gmail.com>
2026-06-17 14:43:16 +0530

    currency

--- config/security.php
+++ config/security.php
@@ -22,7 +22,7 @@ class Security {
         }
         
         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS");
-        header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+        header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, X-CSRF-Token");
         header("Access-Control-Allow-Credentials: true");
         header("Access-Control-Max-Age: 86400");
         
